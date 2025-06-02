38216005c8dd0eccca0cb0ad89051573d4fb0219
Nazar Koliada <1nazaroy@ukr.net>
2025-06-02 22:44:46 +0300

    updated git page

---
+++ src/app/page-layout/main/html-page/html-main-info/html-main-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-html-main-info',
+  imports: [],
+  templateUrl: './html-main-info.component.html',
+  styleUrl: './html-main-info.component.scss'
+})
+export class HtmlMainInfoComponent {
+
+}
--- src/app/page-layout/main/html-page/html-page.component.ts
+++ src/app/page-layout/main/html-page/html-page.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HtmlHeroBannerComponent } from './html-hero-banner/html-hero-banner.component';
 import { HtmlBasicsComponent } from './html-basics/html-basics.component';
-
+import { HtmlMainInfoComponent } from './html-main-info/html-main-info.component';
 
 
 @Component({
   selector: 'app-html-page',
   imports: [
     HtmlHeroBannerComponent,
-    HtmlBasicsComponent
+    HtmlBasicsComponent,
+    HtmlMainInfoComponent
   ],
   templateUrl: './html-page.component.html',
   styleUrl: './html-page.component.scss'
