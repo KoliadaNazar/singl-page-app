029aecf1b78788f09e17aede4137f82cd96d7f41
Nazar Koliada <1nazaroy@ukr.net>
2025-06-07 00:49:30 +0300

    ceated pottfolio on main page

--- src/app/page-layout/main/home-layout/home-layout.component.ts
+++ src/app/page-layout/main/home-layout/home-layout.component.ts
@@ -7,9 +7,8 @@ import { SmallMenuComponent } from './small-menu/small-menu.component';
 import { TechCardsComponent } from './tech-cards/tech-cards.component';
 import { HomeBannerComponent } from './home-banner/home-banner.component';
 import { InfoCardsComponent } from './info-cards/info-cards.component';
-import { ServiceCardsComponent } from './service-cards/service-cards.component';
 import { ServiceBannerComponent } from './service-banner/service-banner.component';
-
+import { PortfolioCardsComponent } from './portfolio-cards/portfolio-cards.component';
 
 
 
@@ -22,8 +21,8 @@ import { ServiceBannerComponent } from './service-banner/service-banner.componen
     TechCardsComponent,
     HomeBannerComponent,
     InfoCardsComponent,
-    ServiceCardsComponent,
-    ServiceBannerComponent
+    ServiceBannerComponent,
+    PortfolioCardsComponent
 
   ],
   templateUrl: './home-layout.component.html',
---
+++ src/app/page-layout/main/home-layout/portfolio-cards/portfolio-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-cards',
+  imports: [],
+  templateUrl: './portfolio-cards.component.html',
+  styleUrl: './portfolio-cards.component.scss'
+})
+export class PortfolioCardsComponent {
+
+}
