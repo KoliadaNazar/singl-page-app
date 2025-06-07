717231d899d6b8d08074edec03ee36d8b5532d41
Nazar Koliada <1nazaroy@ukr.net>
2025-06-07 23:59:19 +0300

    updated home page images and portfolio section

--- src/app/page-layout/main/home-layout/info-cards/info-cards.component.ts
+++ src/app/page-layout/main/home-layout/info-cards/info-cards.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-info-cards',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './info-cards.component.html',
   styleUrl: './info-cards.component.scss'
 })
